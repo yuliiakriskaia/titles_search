7001ef5b13d7c3f64d46222797fd86ae8e5917c6
y.kryzka <y.kryzka@quantumobile.com>
2017-03-22 19:18:16 +0200

    add execution time and elements number information

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 # Searcher
 
 ## Prerequisites:
-You need to use `python2.7`.
+* `python2.7` is required.
+* `Docker` and `docker-compose` are needed for running the app with web UI. Here are the instructions:
+
+https://docs.docker.com/engine/installation/#platform-support-matrix
+https://docs.docker.com/engine/installation/linux/linux-postinstall/
+https://docs.docker.com/compose/install/
+
 
 ## Usage
-To run searcher do the following:
+To run searcher backend do the following:
 ```
 cd titles_search/
 python main.py
 ```
+To run search with web UI make the next steps:
+```
+cd titles_search/
+docker-compose up
+```
+Go to `localhost:3000`.
 
 ## Testing
 To run tests make the next steps:
--- client/src/components/BookSearch.js
+++ client/src/components/BookSearch.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import $ from 'jquery';
-import readCookie from '../readCookie';
 import BookList from './BookList';
 
 var BookSearch = React.createClass({
     getInitialState: function () {
-        return {query: '', books: []};
+        return {query: '', books: [], execution_time: 0, elements_number: 0};
     },
     handleQueryChange: function (e) {
         this.setState({query: e.target.value});
@@ -19,9 +18,13 @@ var BookSearch = React.createClass({
             dataType: 'json',
             cache: false,
             data: JSON.stringify({"query": this.state.query}),
-            success: data => {
+                success: data => {
                 if (!('error' in data)) {
-                    this.setState({books: data['books']});
+                    console.log(data);
+                    this.setState({
+                        books: data['books'],
+                        execution_time: data['execution_time'],
+                        elements_number: data['books'].length});
                 }
             },
             error: (xhr, status, err) => {
@@ -30,24 +33,24 @@ var BookSearch = React.createClass({
         });
     },
     render: function () {
-
         return (
         <div>
             <form className="navbar-form navbar-left" role="search">
-                    <div className="form-group col-xs-12 col-sm-12 col-md-8 col-lg-10">
+                    <div className="form-group col-xs-12 col-sm-12 col-md-4 col-lg-6">
                         <input type="text" className="form-control" placeholder="Search" value={ this.state.query }
                                onChange={ this.handleQueryChange }/>
                     </div>
-                    <div className="form-group">
+                    <div className="form-group col-xs-12 col-sm-12 col-md-4 col-lg-6">
                         <button id="search" type="button" className="btn btn-default" onClick={ this.searchBooks }>
                         Search
                         </button>
                     </div>
-                    <div className={this.state.books===[]?"content":"hidden"}>
+                    <div className="content">
+                        <div className="form-group col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <p>Execution time: { this.state.execution_time } </p>
+                            <p>Elements number: { this.state.elements_number } </p>
+                        </div>
                         <BookList books={this.state.books} />
-                    </div>
-                    <div className={this.state.books===[]?"hidden":"content"}>
-                        No content.
                     </div>
              </form>
         </div>
